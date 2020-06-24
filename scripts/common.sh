f3d8f9927a930cf3a33417d5fbd26244a52ce3e4
jopadan <jopadan@ctemplar.com>
2020-06-24 11:47:55 +0200

    fix urls

--- scripts/common.sh
+++ scripts/common.sh
@@ -82,14 +82,14 @@ export ARM_MPFR_TARBALL_TYPE=`get_make_var arm_mpfr_tarball_type`
 export ARM_MPC_TARBALL_TYPE=`get_make_var arm_mpc_tarball_type`
 export ARM_ISL_TARBALL_TYPE=`get_make_var arm_isl_tarball_type`
 
-export SH_GMP_URL=gmplib.org/download/gmp/gmp-${GMP_VER}.tar.${SH_GMP_TARBALL_TYPE}
-export SH_MPFR_URL=www.mpfr.org/mpfr-current/mpfr-${MPFR_VER}.tar.${SH_MPFR_TARBALL_TYPE}
-export SH_MPC_URL=ftp.gnu.org/gnu/mpc/mpc-${MPC_VER}.tar.${SH_MPC_TARBALL_TYPE}
-export SH_ISL_URL=isl.gforge.inria.fr/isl-${ISL_VER}.tar.${SH_ISL_TARBALL_TYPE}
-export ARM_GMP_URL=gmplib.org/download/gmp/gmp-${GMP_VER}.tar.${ARM_GMP_TARBALL_TYPE}
-export ARM_MPFR_URL=www.mpfr.org/mpfr-current/mpfr-${MPFR_VER}.tar.${ARM_MPFR_TARBALL_TYPE}
-export ARM_MPC_URL=ftp.gnu.org/gnu/mpc/mpc-${MPC_VER}.tar.${ARM_MPC_TARBALL_TYPE}
-export ARM_ISL_URL=isl.gforge.inria.fr/isl-${ISL_VER}.tar.${ARM_ISL_TARBALL_TYPE}
+export SH_GMP_URL=gmplib.org/download/gmp/gmp-${SH_GMP_VER}.tar.${SH_GMP_TARBALL_TYPE}
+export SH_MPFR_URL=www.mpfr.org/mpfr-current/mpfr-${SH_MPFR_VER}.tar.${SH_MPFR_TARBALL_TYPE}
+export SH_MPC_URL=ftp.gnu.org/gnu/mpc/mpc-${SH_MPC_VER}.tar.${SH_MPC_TARBALL_TYPE}
+export SH_ISL_URL=isl.gforge.inria.fr/isl-${SH_ISL_VER}.tar.${SH_ISL_TARBALL_TYPE}
+export ARM_GMP_URL=gmplib.org/download/gmp/gmp-${ARM_GMP_VER}.tar.${ARM_GMP_TARBALL_TYPE}
+export ARM_MPFR_URL=www.mpfr.org/mpfr-current/mpfr-${ARM_MPFR_VER}.tar.${ARM_MPFR_TARBALL_TYPE}
+export ARM_MPC_URL=ftp.gnu.org/gnu/mpc/mpc-${ARM_MPC_VER}.tar.${ARM_MPC_TARBALL_TYPE}
+export ARM_ISL_URL=isl.gforge.inria.fr/isl-${ARM_ISL_VER}.tar.${ARM_ISL_TARBALL_TYPE}
 
 # Retrieve the web downloader program available in this system.
 export IS_CURL=0
